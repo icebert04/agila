321e1b8b4bb88da778c084dc5c20663e3215a064
icebert04 <icebert04@gmail.com>
2020-06-18 14:10:50 +0800

    tenats html

--- src/components/Futer.js
+++ src/components/Futer.js
@@ -18,7 +18,7 @@ export default class Futer extends Component {
               <article className="footers">
               
                   <h5>contact us</h5>
-                    <h6>our team is at your disposal</h6>
+                    <h6>Our team is at your disposal</h6>
                     
              
                   <p><b><FaPhoneAlt/></b>0917-5691002</p>
@@ -40,7 +40,7 @@ export default class Futer extends Component {
                   <h5>
                  Our Philippine Eagle is diminishing
                   </h5>
-                  <p>Help The Philippine Eagle Foundation in your own way</p>
+                  <p>Help the Philippine Eagle Foundation in your own way</p>
                   <a className="btn-secondary btn" href="https://www.philippineeaglefoundation.org/" target="_new">Donate</a>   
          
                   </div>
@@ -68,7 +68,7 @@ export default class Futer extends Component {
                       <div  className= "footers">
                         <span><GrFacebook/></span><h5>Join our exclusive facebook group</h5>
                         
-                        <p>Become part of our substantial & vibrant 'Hosting Community', Get inside access</p>
+                        <p>Become part of our substantial & vibrant "Hosting Community"<br /> Get inside access</p>
                         <a href="https://www.facebook.com/groups/203282164142189/" target="_new" className="btn btn-primary">Join Now</a>
                     </div> 
 
--- src/components/Tenants.js
+++ src/components/Tenants.js
@@ -11,34 +11,7 @@ import { GiTwoCoins, GiLifeSupport } from 'react-icons/gi';
 
 
 export default class Tenants extends Component {
-state={
-    landlord:[
-        {
-            icon:<FaVideo/>,
-            title: "view online",
-            info: "Our team will visit properties, take photos, floor plans and video tours for you, at no cost",
-            subinfo: "We will show you around, a guided tour from the comforts of your home"
-        },
-        {
-            icon:<FaCoffee/>,
-            title: "complete amenities",
-            info: "High-speed WiFi, fully-equipped kitchen, a coffee machine, on-site laundry, gym, working desks/office space & more",
-            subinfo: "Tell us what else do you need... We'll add that too"
-        },
-         {
-            icon:<GiTwoCoins/>,
-            title: "cheaper",
-            info: "Will help you find the right location and a furnished home to fit your need within your budget",
-            subinfo : "Provide more spacious, lower-cost alternative to a hotel"
-        },
-        {
-            icon:<GiLifeSupport/>,
-            title: "Ongoing Support",
-            info: "Avoid face-to-face contact with around the clock support via phone & email",
-            subinfo: "Our live chat is here to manage all critical service needs"
-        }
-    ]
-};
+
 
     render() {
      return (
@@ -46,19 +19,45 @@ state={
         <div className="tenants">
             <Title title='for tenants' />
             <div className="tenants-center">
-                {this.state.landlord.map((item, index) => {
-              return (
-              <article key={index} className="guest">
-              <span>{item.icon}</span>
-              <h6>{item.title}</h6>
-              <p>{item.info}</p>
+              
+             
+              <article className="guest">
+              <span><FaVideo/></span>
+              <h6>view online</h6>
+              <p>Our team will visit properties, take photos, floor plans and video tours for you, at no cost</p>
               <br/>
-              <p>{item.subinfo}</p>
+              <p>We will show you around, a guided tour from the comforts of your home</p>
               </article > 
-              );         
-          })}
-            </div>
+
+               <article className="guest">
+               <span><FaCoffee/></span>
+               <h6>complete amenities</h6>
+               <p>High-speed WiFi, fully-equipped kitchen, a coffee machine, on-site laundry, gym, working desks/office space & more</p>
+               <br/>
+               <p>Tell us what else do you need... We'll add that too</p>
+               </article > 
+
+                <article className="guest">
+                <span><GiTwoCoins/></span>
+                <h6>cheaper</h6>
+                <p>Will help you find the right location and a furnished home to fit your need within your budget</p>
+                <br/>
+                <p>Provide more spacious, lower-cost alternative to a hotel</p>
+                </article > 
+
+                 <article className="guest">
+                 <span><GiLifeSupport/></span>
+                 <h6>Ongoing Support</h6>
+                 <p>Avoid face-to-face contact with around the clock support via phone & email</p>
+                 <br/>
+                 <p>Our live chat is here to manage all critical service needs</p>
+                 </article > 
+
+                </div>
         </div>
+                      
+       
+            
       );
     }
 }
