b311095e6fdeabfd066b13a8bd5e605a33634ddc
icebert04 <icebert04@gmail.com>
2020-06-25 09:37:50 +0800

    description meta

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import defaultImg from '../images/room-2.jpeg';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link } from "react-router-dom";
+import defaultImg from "../images/room-2.jpeg";
+import PropTypes from "prop-types";
 import Verified from "../components/Verified";
 
-export default function Property({property}) {
- const{name,slug,images,price,verified} = property
+export default function Property({ property }) {
+  const { name, slug, images, price, verified } = property;
 
-
-    return (
-        <article className="property">
-            <div className="img-container">
-                <img src={images[0] || defaultImg} alt="single"/>
-                <div className="price-top">
-                    <h6>₱{price}</h6>
-                    <p>per night</p>
-                </div>
-               {verified && <Verified/>}
-                <Link to={`/properties/${slug}`} className="btn-primary property-link" >
-                    Features
-                </Link>
-            </div>
-            <p className="property-info">{name}</p>
-        </article>
-    );
+  return (
+    <article className="property">
+      <div className="img-container">
+        <img src={images[0] || defaultImg} alt="single" />
+        <div className="price-top">
+          <h6>₱{price}</h6>
+          <p>per month</p>
+        </div>
+        {verified && <Verified />}
+        <Link to={`/properties/${slug}`} className="btn-primary property-link">
+          Features
+        </Link>
+      </div>
+      <p className="property-info">{name}</p>
+    </article>
+  );
 }
 
 Property.propTypes = {
-    property:PropTypes.shape({
-        name:PropTypes.string.isRequired,
-        slug:PropTypes.string.isRequired,
-        images:PropTypes.arrayOf(PropTypes.string).isRequired,
-        price:PropTypes.number.isRequired
-    })
-}
+  property: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    price: PropTypes.number.isRequired,
+  }),
+};
